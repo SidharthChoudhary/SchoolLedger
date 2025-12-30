571074f6a46e54846e19c9f9849cf242a5a8c324
School Ledger Dev <dev@schoolledger.local>
2025-12-30 22:24:48 +0000

    Move manual salary data to Ledger

--- schoolapp/settings.py
+++ schoolapp/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Directory where collectstatic will collect static files for production
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
