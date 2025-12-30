f8c44dbf9b1af5ab63ed025aec98595349ddf6f5
School Ledger Dev <dev@schoolledger.local>
2025-12-30 19:04:58 +0000

    Remove mobile webdesign

--- schoolapp/settings.py
+++ schoolapp/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'schoolapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dpstibariyan$schoolledger',
-        'USER': 'dpstibariyan',
-        'PASSWORD': 'schoolledger@2025',  # Set this after MySQL is created
-        'HOST': 'dpstibariyan.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
